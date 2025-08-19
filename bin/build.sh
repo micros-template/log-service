2cb01776d86dd78268b45a37f22c09a9b1239f9d
daffadon <daffaputranarendra9@gmail.com>
2025-08-19 13:12:09 +0700

    chore(script): change build script

--- bin/build.sh
+++ bin/build.sh
@@ -3,26 +3,36 @@
 mkdir -p ./bin/dist
 
 service_name="log_service"
+full_image_name="docker-registry.anandadf.my.id/micros-template/$service_name"
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "./bin/dist/$service_name" ./cmd
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o "./bin/dist/$service_name" ./cmd
+wait
+
+if ! command -v upx >/dev/null 2>&1; then
+  echo "UPX not found. Installing..."
+  apk update && apk add upx
+fi
+
+upx --best --lzma ./bin/dist/$service_name
 wait
 
 VERSION=$(cat VERSION)
 echo "Building Docker image for $service_name:$VERSION" >/dev/stderr
 docker build -t "$service_name:$VERSION" --build-arg BIN_NAME=$service_name -f Dockerfile .
 
-registry_image="$CI_REGISTRY_IMAGE:$VERSION"
-echo "$CI_JOB_TOKEN" | docker login -u gitlab-ci-token --password-stdin "$CI_REGISTRY"
+registry_image="$full_image_name:$VERSION"
+echo "${HARBOR_ROBOT_PASSWORD}" | docker login https://docker-registry.anandadf.my.id -u "${HARBOR_ROBOT_USERNAME}" --password-stdin
 
 echo "Tagging image as $registry_image" >/dev/stderr
 docker tag "$service_name:$VERSION" "$registry_image"
 docker push "$registry_image"
 
-echo "Tagging image as $registry_image" >/dev/stderr
-docker tag "$service_name:$VERSION" "$CI_REGISTRY_IMAGE:latest"
-docker push "$CI_REGISTRY_IMAGE:latest"
+echo "Tagging image as $full_image_name:latest" >/dev/stderr
+docker tag "$service_name:$VERSION" "$full_image_name:latest"
+docker push "$full_image_name:latest"
 
 echo "Removing local image and tagged image" >/dev/stderr
 docker rmi "$service_name:$VERSION"
 docker rmi "$registry_image"
-docker rmi "$CI_REGISTRY_IMAGE:latest"
+docker rmi "$full_image_name:latest"
